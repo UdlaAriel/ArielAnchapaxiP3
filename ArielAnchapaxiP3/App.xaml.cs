87a671a98b8bb0817bc3f4bb361bf299663b54e7
Josue Riera <josueriera23@hotmail.com>
2025-01-24 07:12:58 -0500

    Agregar archivos de proyecto.

---
+++ ArielAnchapaxiP3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ArielAnchapaxiP3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
